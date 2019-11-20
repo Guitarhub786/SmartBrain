9776ac47a09b6673984630726e96d087d9836710
Guitarhub786 <codewithdrawal@gmail.com>
2019-11-20 11:39:28 +0000

    fixed bug missing { } in  ImageURL

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import './App.css';
 const app = new Clarifai.App({
   // insert your api key here
 
-  apiKey: ' !!!input api here!!! '
+  apiKey: ' insert api here '
 });
 
 const particlesOptions = {
@@ -47,7 +47,7 @@ class App extends Component {
       .then(
         function (response) {
           // do something with response
-          console.log(response);
+          console.log(response.outputs[0].data.regions[0].region_info.bounding_box);
         },
         function (err) {
           // there was an error
